efef2edec6b49adcce748dd6e69a7374d1a13b94
abdullah <abdullah.karkarli@ogr.iu.edu.tr>
2023-03-18 16:02:49 +0300

    Done

--- index.js
+++ index.js
@@ -18,3 +18,9 @@ function appendCat(name){
 function prependCat(name){
     return [name,...cats];
 }
+function removeLastCat(){
+    return [...cats.slice(0,cats.length-1)];
+}
+function removeFirstCat(){
+    return [...cats.slice(1)];
+}
